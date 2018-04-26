73bfb7a537ddf6a4b56848f1696cf783b6c537d0
Shahrukh <kmshahruk@gmail.com>
2018-04-26 15:00:40 -0400

    Updated student reg
    updated student registration

--- StudentRegistration.php
+++ StudentRegistration.php
@@ -3,43 +3,76 @@ require ("htmlStructure.php");
 require_once ("connectDB.php");
 
 $body = <<<BODY
-    <form action='$_SERVER[PHP_SELF]' method="post">
-    <h1>Student Registration</h1>
-    
-    <strong>Email: </strong><input type="email" required><br><br>
-    
-    <strong>Password: </strong><input type="password" required><br><br>
-    
-    <strong>Re-Enter Password: </strong><input type="password" required><br><br>
-    
-    <strong>First Name: </strong><input type="text" required><br><br>
-    
-    <strong>Last Name: </strong><input type="text" required><br><br>
-    
-    <strong>GPA: </strong><input type="number" step="0.01" name="gpa" min="0" max="4" required><br><br>   
-    
-    <strong>UID: </strong><input type="text" name="uid" required><br><br>
-    
-    <strong>Have you TA'd before? </strong>&nbsp;<input type="radio" name="taB4" required>Yes &nbsp;<input type="radio" name="taB4" required>No<br><br>
-    
-    <strong>Number of Semesters Ta'd? </strong><input type="number" name="numOfSemesters" min="0" required><br><br>
-    
-    <strong>Are you a graduate student?</strong>&nbsp;<input type="radio" name="grad" required>Yes &nbsp;<input type="radio" name="grad" required>No<br><br>
-    
-    <strong>Courses Interested TA'ing for</strong>
-    <select name="studyType">
-        <option value="Course 1">Course 1</option>
-        <option value="Course 2">Course 2</option>
-    </select><br><br>
-    
-    <strong>Resume</strong><input type="file" name="resumeID" required><br><br>
-    
-    <strong>Transcript</strong><input type="file" name="transcriptID" required><br><br>
-    
 
-    
-
-    </form>
+     <div class="container">
+        <h1>Student Registration</h1>
+        <form action='$_SERVER[PHP_SELF]' method="post">
+        
+            <div class="form-group">
+                <label for="email">Email: </label>
+                <input type="email" name="studentEmail" class="form-control" id="email" placeholder="Enter email" required><br><br>
+            </div>
+            
+            <div class="form-group">
+                <label for="password">Password: </label>
+                <input type="password" name="studentPassword" class="form-control" id="password" placeholder="Enter password" required><br><br>
+            </div>
+            
+            <div class="form-group">
+                <label for="rePassword">Re-enter Password: </label>
+                <input type="password" name="rePassword" class="form-control" id="rePassword" placeholder="Re-Enter password" required><br><br>
+            </div>
+            
+            <div class="form-group">
+                <label for="firstName">First Name: </label>
+                <input type="text" name="firstName" class="form-control" id="firstName" placeholder="First Name" required><br><br>
+            </div>
+            
+            <div class="form-group">
+                <label for="lastName">Last Name: </label>
+                <input type="text" name="lastName" class="form-control" id="lastName" placeholder="Last Name" required><br><br>
+            </div>
+            
+            <div class="form-group">
+                <label for="gpa">GPA: </label>
+                <input type="number" step="0.01" min="0" max="4" name="gpa" class="form-control" id="gpa" placeholder="GPA" required><br><br>
+            </div>
+            
+            <div class="form-group">
+                <label for="UID">UID: </label>
+                <input type="text" name="uid" class="form-control" id="uid" placeholder="UID" required><br><br>
+            </div>
+            
+            <div class="form-group">
+                <label for="taB4">Have you TA'd before? </label>
+                <input type="radio" name="taB4" class="form-control" id="taB4" required>Yes
+                <input type="radio" name="taB4" class="form-control" required>No<br><br>
+            </div>
+            
+             <div class="form-group">
+                <label for="numOfSemesters">Number of Semesters Ta'd? </label>
+                <input type="number" name="numOfSemesters" min="0" class="form-control" id="numOfSemesters" required>
+            </div>
+            
+            <div class="form-group">
+                <label for="grad">Are you a graduate student? </label>
+                <input type="radio" name="grad" class="form-control" id="grad" required>Yes
+                <input type="radio" name="grad" class="form-control" required>No<br><br>
+            </div>
+            
+            
+            <div class="form-group">
+                <label for="resume">Resume: </label>
+                <input type="file" name="resumeID" class="form-control" id="resume" required><br><br>
+            </div>
+            
+            <div class="form-group">
+                <label for="transcript">Transcript: </label>
+                <input type="file" name="transcriptID" class="form-control" id="transcript" required><br><br>
+            </div>
+            
+        </form>
+    </div>
 BODY;
 
 echo generatePage($body);
