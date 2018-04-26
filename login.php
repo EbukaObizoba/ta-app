82eefba47ab49551c40c1fa532725fba890ce650
Steven Sombat <sombatsteven@gmail.com>
2018-04-26 14:41:05 -0400

    bootstrap login
    added bootstrap for login

--- login.php
+++ login.php
@@ -11,14 +11,37 @@
 
 
     $body = <<<BODY
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />	
+    
+    </head>
+    
+    <body>
+    <div class="container">
      <form action="loginVerify.php" method="post">
-        <strong>Email</strong>: <input type="email" name="email" required> <br>
-        <strong>Password</strong>: <input type="password" name="password" required>  <br>
-        <br>
-        <input type="submit" name="submit" value="Login">  
-        <br>
-        <br>
-        <a href="registrationPage.php"> Not registered? Click here</a> 
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" name="email" required class="form-control" id="email">
+        </div>
+        <div class="form-group">
+            <label for="email">Password</label>
+            <input type="password" name="password" required class="form-control" id="password">
+        </div>
+        
+        <div class="form-group">
+            <input type="submit" name="submit" value="Login" class="form-control"> 
+        </div> 
+        </form>
+        <a href="registrationPage.php" class="form-control" style="text-align:center"> Not registered? Click here</a> 
+    </div>
+
+    <script
+          src="http://code.jquery.com/jquery-3.3.1.min.js"
+          integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+          crossorigin="anonymous">
+        </script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </body>
 BODY;
     
     
