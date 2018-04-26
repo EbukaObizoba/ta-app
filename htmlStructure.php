77d9acc44a42651e5f6a7d7aef29ab709b86a9f1
Chukwuebuka Obizoba <cobizoba@umd.edu>
2018-04-26 17:25:45 -0400

    Added file uploads table to the student profile page

--- htmlStructure.php
+++ htmlStructure.php
@@ -8,12 +8,13 @@ function generatePage($body) {
         <title>ta-app</title>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
         <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1">	
     </head>
             
     <body>
-    <div class="container-fluid">
+    <div class="container">
             $body
     </div>
     <script src="bootstrap/jquery-3.2.1.min.js"></script>
--- studentProfile.php
+++ studentProfile.php
@@ -7,8 +7,18 @@
     $entry = $_SESSION["profile"];
     $body =<<< EOBODY
         <form class="form-group">
-            <div class="form-group">
+            <div class="form-group" method="post">
                 <h1>Student Name:{$entry["fName"]} {$entry["lName"]}</h1>
+            </div>
+            <div>
+                <h3>Upload student documents:</h3><br>
+                <table border="2">
+                    <thead>Upload student documents:</thead>
+                    <tr><td>Document type</td><td>Document selection</td></tr>
+                    <tr><td>Resume</td><td><input type="file" name="resumeFileName" accept=".pdf"></td></tr>
+                    <tr><td>Unofficial Transcript</td><td><input type="file" name="transcriptFileName" accept=".pdf"></td></tr>
+                </table>
+                <input type="submit" name="submitDocuments">
             </div>
         </form>
 EOBODY;
