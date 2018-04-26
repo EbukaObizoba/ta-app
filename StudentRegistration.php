8520bf2e9233a84ad0767c5cfc82bc31f67c2d41
Shahrukh <kmshahruk@gmail.com>
2018-04-26 17:23:17 -0400

    updated radio
    Updated form visuals and bootstrap

--- StudentRegistration.php
+++ StudentRegistration.php
@@ -5,48 +5,50 @@ require_once ("connectDB.php");
 $body = <<<BODY
 
      <div class="container">
-        <h1>Student Registration</h1>
+        <h1>Student Registration</h1><br>
         <form action="addStudent.php" method="post">
         
             <div class="form-group">
                 <label for="email">Email: </label>
-                <input type="email" name="studentEmail" class="form-control" id="email" placeholder="Enter email" required><br><br>
+                <input type="email" name="studentEmail" class="form-control" id="email" placeholder="Enter email" required><br>
             </div>
             
             <div class="form-group">
                 <label for="password">Password: </label>
-                <input type="password" name="studentPassword" class="form-control" id="password" placeholder="Enter password" required><br><br>
+                <input type="password" name="studentPassword" class="form-control" id="password" placeholder="Enter password" required><br>
             </div>
             
             <div class="form-group">
                 <label for="rePassword">Re-enter Password: </label>
-                <input type="password" name="rePassword" class="form-control" id="rePassword" placeholder="Re-Enter password" required><br><br>
+                <input type="password" name="rePassword" class="form-control" id="rePassword" placeholder="Re-Enter password" required><br>
             </div>
             
             <div class="form-group">
                 <label for="firstName">First Name: </label>
-                <input type="text" name="firstName" class="form-control" id="firstName" placeholder="First Name" required><br><br>
+                <input type="text" name="firstName" class="form-control" id="firstName" placeholder="First Name" required><br>
             </div>
             
             <div class="form-group">
                 <label for="lastName">Last Name: </label>
-                <input type="text" name="lastName" class="form-control" id="lastName" placeholder="Last Name" required><br><br>
+                <input type="text" name="lastName" class="form-control" id="lastName" placeholder="Last Name" required><br>
             </div>
             
             <div class="form-group">
                 <label for="gpa">GPA: </label>
-                <input type="number" step="0.01" min="0" max="4" name="gpa" class="form-control" id="gpa" placeholder="GPA" required><br><br>
+                <input type="number" step="0.01" min="0" max="4" name="gpa" class="form-control" id="gpa" placeholder="GPA" required><br>
             </div>
             
             <div class="form-group">
                 <label for="UID">UID: </label>
-                <input type="text" name="uid" class="form-control" id="uid" placeholder="UID" required><br><br>
+                <input type="text" name="uid" class="form-control" id="uid" placeholder="UID" required><br>
             </div>
             
-            <div class="form-group">
-                <label for="taB4">Have you TA'd before? </label>
-                <input type="radio" name="taB4" class="form-control" id="taB4" value="Y" required>Yes
-                <input type="radio" name="taB4" class="form-control" value="N" required>No<br><br>
+            <div class="form-group" class="radio-inline">
+                <label for="taB4">Have you TA'd before? </label><br>
+                <em>Yes</em>
+                <input class="radio-inline" type="radio" name="taB4" class="form-control" id="taB4" value="Y" required>
+                <em>No</em>
+                <input class="radio-inline" type="radio" name="taB4" class="form-control" value="N" required><br>
             </div>
             
              <div class="form-group">
@@ -55,9 +57,11 @@ $body = <<<BODY
             </div>
             
             <div class="form-group">
-                <label for="grad">Are you a graduate student? </label>
-                <input type="radio" name="grad" class="form-control" id="grad" value="grad" required>Yes
-                <input type="radio" name="grad" class="form-control" value="undergrad" required>No<br><br>
+                <label for="grad">Are you a graduate student? </label><br>
+                <em>Yes</em>
+                <input class="radio-inline" type="radio" name="grad" class="form-control" id="grad" value="grad" required>
+                <em>No</em>
+                <input class="radio-inline" type="radio" name="grad" class="form-control" value="undergrad" required><br>
             </div>
             
             
--- htmlStructure.php
+++ htmlStructure.php
@@ -8,7 +8,7 @@ function generatePage($body) {
         <title>ta-app</title>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />	
         <meta name="viewport" content="width=device-width, initial-scale=1">	
     </head>
             
@@ -16,8 +16,12 @@ function generatePage($body) {
     <div class="container-fluid">
             $body
     </div>
-    <script src="bootstrap/jquery-3.2.1.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script
+          src="http://code.jquery.com/jquery-3.3.1.min.js"
+          integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+          crossorigin="anonymous">
+        </script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </body>
 </html>
 MAIN;
