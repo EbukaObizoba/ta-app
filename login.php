bfdc8d079f5562b776b4d76f100dc229e24bf4c2
Steven Sombat <sombatsteven@gmail.com>
2018-04-26 12:29:24 -0400

    Setup
    x.html is the main page that will be changed to index.html when done.
    Setup webpage direction

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sombaat
+ * Date: 4/26/2018
+ * Time: 10:51 AM
+ */
+
+    session_start();
+
+
+
+    $body = <<<BODY
+     <form action="loginVer" method="post">
+        <strong>Email</strong>: <input type="email" name="email" required> <br>
+        <strong>Password</strong>: <input type="password" name="password" required>  <br>
+        <br>
+        <input type="submit" name="submit" value="Login">  
+        <br>
+        <br>
+        <a href="registrationPage.php"> Not registered? Click here</a> 
+BODY;
+    
+    
+    echo $body;
+    
