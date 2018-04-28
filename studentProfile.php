57ef9f6416f6738ac2f968b7d3c6b1b809d774ef
Chukwuebuka Obizoba <cobizoba@umd.edu>
2018-04-27 20:17:54 -0400

    Added ability for student to choose courses to TA.

--- studentProfile.php
+++ studentProfile.php
@@ -5,10 +5,19 @@
     if(session_status() == PHP_SESSION_NONE) session_start();
     $db = connectToDB();
     $entry = $_SESSION["profile"];
+    $courses = getCourses($db);
+    $coursesBody = buildCoursesHTML($courses);
     $body =<<< EOBODY
-        <form class="form-group">
-            <div class="form-group" method="post">
-                <h1>Student Name:{$entry["fName"]} {$entry["lName"]}</h1>
+        <form class="form-group" method="get">
+            <div class="form-group">
+                <h1>{$entry["fName"]} {$entry["lName"]}</h1>
+            </div>
+            <div>
+                <h3>Choose courses to TA:</h3><br>
+                <div >
+                    $coursesBody
+                </div>
+                <input type="submit" name="coursesToTutor" value="Submit Intended Courses"/>
             </div>
             <div>
                 <h3>Upload student documents:</h3><br>
@@ -20,16 +29,64 @@
                     <div class="col-md-2">Unofficial Transcript</div>
                     <div class="col-md-3"><input type="file" name="transcriptFileName" accept=".pdf"></div>
                 </div>
-                <!--<table border="2">
-                    <thead>Upload student documents:</thead>
-                    <tr><td>Document type</td><td>Document selection</td></tr>
-                    <tr><td>Resume</td><td></td></tr>
-                    <tr><td>Unofficial Transcript</td><td></td></tr>
-                </table>-->
-                <input type="submit" name="submitDocuments">
-                <input type="button" name="logout" value="">
+                <input type="button" name="submitDocuments" value="Submit Documents">
             </div>
         </form>
 EOBODY;
+    //print_r($_GET);
+    if(isset($_GET["coursesToTutor"])){
+        $chosenCourses = [];
+        foreach($courses as $course){
+            if(isset($_GET[$course])){
+                array_push($chosenCourses, substr($course, 4));
+            }
+        }
+        $coursesAsString = (count($chosenCourses) > 0) ? implode(",",$chosenCourses) : NULL;
+        // Now that we have the chosen courses, the student table can be updated.
+        $table = "studenttable";
+        $field = "courseIDs";
+        $email = $entry["email"];
+        $query = "update $table set courseIDs='$coursesAsString' where email='$email'";
+        echo $query;
+        $result = $db->query($query);
+        if (!$result) {
+            die("Update failed: ". $db->error);
+        } else echo "\nSuccessful courses update!\n";
+
+    }
+
+    function getCourses($db){
+        $table = "coursetable";
+        $query = "select course from $table";
+        $result = $db->query($query);
+        $courses = [];
+        if (!$result) {
+            die("Retrieval failed: ". $db->error);
+        } else if($result->num_rows == 0) {
+            echo "No course entries exist in the database.";
+        } else {
+            $num_rows = $result->num_rows;
+            for ($row_index = 0; $row_index < $num_rows; $row_index++) {
+                $result->data_seek($row_index);
+                $row = $result->fetch_array(MYSQLI_ASSOC);
+                array_push($courses, "CMSC" . $row["course"]);
+            }
+        }
+        return $courses;
+    }
+
+    function buildCoursesHTML($courses){
+        $body = "";
+        foreach((array) $courses as $course){
+            $boxes = keepBoxesChecked($course);
+            $body .= "$course&nbsp;<input type='checkbox' name=$course $boxes /><br>";
+        }
+        return $body;
+    }
+    function keepBoxesChecked($course){
+        if(isset($_GET[$course])){
+            return "checked";
+        } else "";
+    }
 
     echo generatePage($body);
